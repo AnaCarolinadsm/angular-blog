8581ca4fecdc8799f2b8ca2c42b3abff3c11a3d9
ana <anadsm00@gmail.com>
2023-10-01 16:50:25 -0400

    content update

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,6 +10,19 @@ export const dataFake = [
         "title": "ChatGPT agora pode navegar na internet",
         "description": "Pellentesque dui purus, pharetra eu cursus eget, tempor lobortis sem. Praesent id erat auctor, egestas ipsum vel, efficitur lorem. Praesent pretium at metus et posuere. In et laoreet lacus.",
         "photoCover": "https://www.cienciaedados.com/wp-content/uploads/2023/04/Por-Que-o-ChatGPT-Nao-e-Tao-Inteligente-Quanto-Muitos-Acreditam.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Meta AI | Tudo sobre a IA de WhatsApp, Facebook e Instagram",
+        "description": "Pellentesque dui purus, pharetra eu cursus eget, tempor lobortis sem. Praesent id erat auctor, egestas ipsum vel, efficitur lorem. Praesent pretium at metus et posuere. In et laoreet lacus.",
+        "photoCover": "https://t.ctcdn.com.br/3UyjrTSaTGYDLmeIsTi17cnrA58=/768x432/smart/i804513.jpeg"
+    },
+    {
+        "id": "4",
+        "title": "Meta lança óculos Ray-Ban com transmissão via Facebook e assistente de IA",
+        "description": "Pellentesque dui purus, pharetra eu cursus eget, tempor lobortis sem. Praesent id erat auctor, egestas ipsum vel, efficitur lorem. Praesent pretium at metus et posuere. In et laoreet lacus.",
+        "photoCover": "https://www.hardware.com.br/wp-content/uploads/2023/09/7-4.jpg"
     }
+    
     
 ]
