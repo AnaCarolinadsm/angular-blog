5d702bd26faaa749f12062feaa236c0d3b35cd76
ana <anadsm00@gmail.com>
2023-10-01 16:40:47 -0400

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "A NOVA VERSÃO DO ANGULAR",
+        "description": "Pellentesque dui purus, pharetra eu cursus eget, tempor lobortis sem. Praesent id erat auctor, egestas ipsum vel, efficitur lorem. Praesent pretium at metus et posuere. In et laoreet lacus.",
+        "photoCover": "https://miro.medium.com/v2/resize:fit:1400/1*qgo4_MwETtxdspEqt12sNA.png"
+    },
+    {
+        "id": "2",
+        "title": "ChatGPT agora pode navegar na internet",
+        "description": "Pellentesque dui purus, pharetra eu cursus eget, tempor lobortis sem. Praesent id erat auctor, egestas ipsum vel, efficitur lorem. Praesent pretium at metus et posuere. In et laoreet lacus.",
+        "photoCover": "https://www.cienciaedados.com/wp-content/uploads/2023/04/Por-Que-o-ChatGPT-Nao-e-Tao-Inteligente-Quanto-Muitos-Acreditam.jpg"
+    }
+    
+]
